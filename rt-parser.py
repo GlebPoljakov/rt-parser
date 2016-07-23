f54830b4153faeb9854d218fd6d72de14a4db6a6
Gleb Poljakov <gleb.poljakov@gmail.com>
2016-07-23 19:58:20 +0300

    Click complited. Adding comparison functionality.

--- rt-parser.py
+++ rt-parser.py
@@ -21,8 +21,10 @@ def str2hexstr(inStr):
     return ":".join("{:02x}".format(ord(c)) for c in inStr)
 
 
-def parseRoutingTableHuaweiCE():
-    inFile = sys.stdin
+def parseRoutingTableHuaweiCE(inFile):
+    """
+        Parsing dumps of 'display ip routing' and 'display ip routing all-vpn' of Huawei CE12800 and may be of other Huawei CE devices.
+    """
 
     allRoutingTables = []
 
@@ -42,20 +44,31 @@ def parseRoutingTableHuaweiCE():
         lineCounter = lineCounter + 1
         line = line.strip('\n\r')       # Strip linendings
 
-        #print 'Current Line:%s \n\tHex: %s' % (line, str2hexstr(line))
+        if gDebug >= 3:
+            print 'Readed line:%s' % line
+        if gDebug >= 4:
+            print '\tHex: %s' % str2hexstr(line)
 
         #Skip empty lines
         if line.strip() == '': continue
 
         # Is its new VPN-Instance Routing Table?
         if line == rtDelimiter:
+            if gDebug >=3:
+                print 'Get rtDelimiter on input.'
+
             #append resulting RT-List with new instance of VPN and VPN routes, do it only if it is not a first iteration.
             if thisRTName != '':
+                if gDebug >= 3:
+                    print 'Append Routing Table %s to allRoutingTables.' % thisRTName
                 allRoutingTables.append({'Name': thisRTName, 'RouteRecords': thisRTLines })
 
             # Read new VPN-instance name
-            line = sys.stdin.readline()
-            #print '\tSearch for VPN-Name: "%s" and Hex: "%s"' % (line, str2hexstr(line))
+            line = inFile.readline()
+
+            if gDebug >= 3:
+                print '\tSearch for VPN-Name: "%s" and Hex: "%s"' % (line, str2hexstr(line))
+
             lineCounter = lineCounter + 1
 
             if 'Routing Table' not in line:
@@ -65,13 +78,17 @@ def parseRoutingTableHuaweiCE():
             thisRTName = line[16:].strip()
             thisRTLines = {}
 
-            #print 'New RT: %s' % thisRTName
+            if gDebug >=3:
+                print 'New RT: %s' % thisRTName
 
             # Skip header lines
             for x in xrange(3):
                 line = inFile.readline().strip()
                 lineCounter = lineCounter + 1
-                #print 'SKIP - Line: "%s" and Hex: "%s"' % (line, str2hexstr(line))
+
+                if gDebug >= 3:
+                    print 'SKIP - Line: "%s"' % line
+                    print '\tHex: "%s"' % str2hexstr(line)
 
             lineCounter = lineCounter + 1
             continue
@@ -138,25 +155,50 @@ def getRoute(vpn, prefix):
 
     return res
 
+def printYaml():
+    print 'printYaml'
+    print yaml.dump(allRoutingTables, default_flow_style=False)
+
+def printPlain():
+    print 'printPlain'
+
+def printHtml():
+    print 'printHtml'
+
 @click.command()
-@click.argument('asis', type=click.File('r'))
-@click.argument('tobe', type=click.File('r'))
-def cli(asis, tobe):
+@click.option('--output', 'outputFormat', type=click.Choice(['plain', 'html']), default='html', help='Output format')
+@click.argument('rtdump1', type=click.File('r'))
+@click.argument('rtdump2', type=click.File('r'))
+@click.option('-d', '--debug', count=True)
+def cli(outputFormat, rtdump1, rtdump2, debug):
+    """
+        Comparison of routing table dump from 'rtdump1' file with dump from 'rtdump2' file.
+        rtdump1 - is a TOBE-state of routing table.
+        rtdump2 - is ASIS-state.
+    """
+    if debug:
+        click.echo('outputFormat is:%s' % outputFormat)
+        click.echo('asis is:%s' % rtdump1)
+        click.echo('tobe is:%s' % rtdump2)
 
-    print 'asis is:%s' % asis
-    print 'tobe is:%s' % tobe
+    global gDebug
+    gDebug = debug
 
-    quit()
-
-    global allRoutingTables
-    allRoutingTables = parseRoutingTableHuaweiCE()
+    allRoutingTablesAsis = parseRoutingTableHuaweiCE(rtdump2)
+    allRoutingTablesTobe = parseRoutingTableHuaweiCE(rtdump1)
 
     # Print 
-    printYAML = True
-    printAllRoutes = True
+    doOutput = {
+        'yaml': printYaml,
+        'plain': printPlain,
+        'html': printHtml,
+    }
 
-    if printYAML:
-        print yaml.dump(allRoutingTables, default_flow_style=False)
+    doOutput.get(outputFormat, 'plain')()
+
+    printAllRoutes = debug >= 2
+
+    quit()
 
     for RT in allRoutingTables:
         print 'VPN: %s \t\t Routes: %s' % (RT['Name'], len(RT['RouteRecords']))
@@ -167,7 +209,11 @@ def cli(asis, tobe):
             except KeyError:
                 pass
 
-            #for line in RT['RouteRecords']:
-                #print '%s\t%s' % (line['Prefix'], line['Interface'])
-#            print tabulate(RT['RouteRecords'])
+        if RT['Name'] in ['MGMT', 'AD' ]:
+            for rec, recval in RT['RouteRecords'].items():
+                print rec
+                print '\t%s' % recval
+
+if __name__ == '__main__':
+    cli()
 
